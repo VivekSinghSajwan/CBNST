fd9682061fce2e8669e72e79792ee99c950fc941
Vivek Singh Sajwan <161462899+VivekSinghSajwan@users.noreply.github.com>
2024-12-07 11:49:04 +0530

    Update BankersAlgo.c

--- BankersAlgo.c
+++ BankersAlgo.c
@@ -3,115 +3,97 @@
 
 int n, m;
 
-// Apply safety algorithm to check if a safe sequence exists
 bool applySafetyAlgo(int max[n][m], int allocated[n][m], int need[n][m], int available[m], int safeSequence[n]) 
 {
     bool finish[n];  // Array to track whether a process has finished
     int work[m];  // Work array to simulate resource availability during the algorithm
-
-    // Initialize work array as a copy of available resources
-    for (int i = 0; i < m; i++)
+    for(int i=0; i<m; i++)
         work[i] = available[i];
-
-    // Initialize all processes as unfinished
-    for (int i = 0; i < n; i++) 
+    for(int i=0; i<n; i++) 
         finish[i] = false;
 
-    int k = 0;  // Index for the safe sequence
+    int k = 0;
     bool proceed = true;
-
-    // Main loop to find safe sequence
-    while (proceed) 
+    while(proceed) 
     {
         proceed = false;
-        for (int i = 0; i < n; i++) 
+        for(int i=0; i<n; i++) 
         {
             bool flag = true;
-            if (!finish[i])  // Only check unfinished processes
+            if(finish[i] == false)
             {
-                // Check if the process can be executed (Need <= Available)
-                for (int j = 0; j < m; j++) 
+                for(int j=0; j<m; j++) 
                 {
-                    if (need[i][j] > work[j]) 
+                    if(need[i][j] > work[j]) 
                     {
-                        flag = false;  // If need is greater, the process cannot proceed
+                        flag = false;
                         break;
                     }
                 }
-
-                // If the process can proceed
-                if (flag) 
+                if(flag) 
                 {
                     // Update work (release the resources)
                     for (int j = 0; j < m; j++)
                         work[j] += allocated[i][j];
-
-                    finish[i] = true;  // Mark the process as finished
-                    safeSequence[k++] = i;  // Add the process to the safe sequence
-                    proceed = true;  // At least one process has been executed, so continue
+                    finish[i] = true;
+                    safeSequence[k++] = i;
+                    proceed = true;
                 }
             }
         }
     }
 
-    // Check if all processes have finished
-    for (int i = 0; i < n; i++) 
+    for(int i=0; i<n; i++) 
     {
         if (!finish[i]) 
-            return false;  // If any process is unfinished, the state is unsafe
+            return false;
     }
-
-    return true;  // All processes finished, safe state
+    return true;
 }
-
-// Checks if the state is safe
-bool isSafeState(int max[n][m], int allocated[n][m], int need[n][m], int available[m], int safeSequence[n]) 
-{
-    return applySafetyAlgo(max, allocated, need, available, safeSequence);
-}
-
 int main() 
 {
     printf("Enter number of processes: ");
     scanf("%d", &n);
     printf("Enter number of resources: ");
     scanf("%d", &m);
-
-    int max[n][m], allocated[n][m], need[n][m], available[m], safeSequence[n];
-
+    int max[n][m], allocated[n][m], need[n][m], available[m], total[m], safeSequence[n];
     printf("Enter maximum requirement for %d processes:\n", n);
-    for (int i = 0; i < n; i++) 
+    for(int i=0; i<n; i++) 
     {
-        for (int j = 0; j < m; j++) 
+        for(int j=0; j<m; j++) 
             scanf("%d", &max[i][j]);
     }
-
-    printf("Enter allocated resources for %d processes:\n", n);
-    for (int i = 0; i < n; i++) 
+    printf("Enter allocated resources for %d processes:\n",n);
+    for(int i=0; i<n; i++) 
     {
-        for (int j = 0; j < m; j++) 
+        for (int j=0; j<m; j++) 
         {
-            scanf("%d", &allocated[i][j]);
-            need[i][j] = max[i][j] - allocated[i][j];  // Need = Max - Allocated
+            scanf("%d",&allocated[i][j]);
+            need[i][j] = max[i][j] - allocated[i][j];
         }
     }
+    printf("Enter total resources: ");
+    for(int i=0; i<m; i++) 
+        scanf("%d", &total[i]);
 
-    printf("Enter available resources: ");
-    for (int i = 0; i < m; i++) 
-        scanf("%d", &available[i]);
-
-    // Check if the system is in a safe state
-    if (isSafeState(max, allocated, need, available, safeSequence)) 
+    //calculating sum of allocated resource
+    int allocatedSum[m];
+    for(int i=0; i<m; i++) 
+    {
+        allocatedSum[i] = 0;
+        for (int j=0; j<n; j++) 
+            allocatedSum[i] += allocated[j][i];
+        available[i] = total[i]-allocatedSum[i];
+    }
+    //-----
+    if(applySafetyAlgo(max, allocated, need, available, safeSequence)) 
     {
         printf("\nSystem is in a SAFE state.\n");
         printf("Safe sequence is: ");
-        for (int i = 0; i < n; i++)
+        for(int i=0; i<n; i++)
             printf("P[%d] ", safeSequence[i]);
     } 
     else 
-    {
         printf("\nSystem is NOT in a safe state.\n");
-    }
-
     return 0;
 }
